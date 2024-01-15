af985ea1e8289810c9e0236554a3cf99e22a4e1e
vishwas <vishwas452003@gmail.com>
2024-01-15 14:17:43 +0530

    last update

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ function choice1() {
 }
 function choice2() {
   console.log("clicked on Imperial");
-  document.querySelector(".cm").innerHTML = "inch";
-  document.querySelector(".kg").innerHTML = "pound";
+  document.querySelector(".cm").innerText = "inch";
+  document.querySelector(".kg").innerText = "pound";
   choice = 1;
   console.log("choice is",choice);
   // run2();
@@ -26,7 +26,8 @@ function run(event) {
     console.log(height.value, weight.value);
     var bmi = weight.value / (heightInM * heightInM);
     console.log("BMI is:", bmi);
-    document.querySelector(".output").innerHTML = "Your BMI is " + bmi.toFixed(1);
+    change(bmi);
+    // document.querySelector(".output").innerHTML = "Your BMI is " + bmi.toFixed(1);
   }
   else if(choice === 1){
     var height = document.querySelector(".height");
@@ -34,6 +35,21 @@ function run(event) {
     console.log(height.value, weight.value);
     var bmi = 703 * (weight.value / (height.value * height.value));
     console.log("BMI is:", bmi);
-    document.querySelector(".output").innerHTML = bmi.toFixed(1);
+    document.querySelector(".output").innerHTML =  "Your BMI is " +bmi.toFixed(1);
+    change(bmi);
+  }
+}
+function change(out){
+  if(out < 18.5 ){
+    document.querySelector(".output").innerHTML = "Your BMI is "+out.toFixed(1)+" which says that you are Underweight";
+  }
+  else if(out >= 18.5 && out <=24.9){
+    document.querySelector(".output").innerHTML = "Your BMI is "+out.toFixed(1)+" which says that you are Normal";
+  }
+  else if(out>=25 && out <=29.9){
+    document.querySelector(".output").innerHTML = "Your BMI is "+out.toFixed(1)+" which says that you are Overweight";
+  }
+  else if(out >= 30){
+    document.querySelector(".output").innerHTML = "Your BMI is "+out.toFixed(1)+" which says that you are Obese";
   }
 }
